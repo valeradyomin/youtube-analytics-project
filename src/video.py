7ee5987793774a41bbfda48849584e4139550e74
Valera Demin <wahrwolffe@gmail.com>
2023-10-04 23:49:39 +0300

    HW6: broken_video_id exception ready

--- src/video.py
+++ src/video.py
@@ -10,11 +10,17 @@ class Video:
 
     def __init__(self, id_video: str):
         self.id_video = id_video
-        self.info_video = self.get_video_info(id_video)
-        self.title_video = self.info_video["snippet"]["title"]
-        self.url_video = f"https://www.youtube.com/watch?v={self.id_video}"
-        self.watched_count = self.info_video["statistics"]["viewCount"]
-        self.liked_count = self.info_video["statistics"]["likeCount"]
+        try:
+            self.info_video = self.get_video_info(id_video)
+        except LookupError:
+            print("Видео id не существует.")
+            self.info_video = self.title = self.url_video = self.watched_count = self.like_count = None
+        else:
+            self.info_video = self.get_video_info(id_video)
+            self.title = self.info_video["snippet"]["title"]
+            self.url_video = f"https://www.youtube.com/watch?v={self.id_video}"
+            self.watched_count = self.info_video["statistics"]["viewCount"]
+            self.like_count = self.info_video["statistics"]["likeCount"]
 
     @classmethod
     def get_service(cls):
@@ -29,7 +35,7 @@ class Video:
 
     def __str__(self):
         """Метод для отображения информации об объекте класса для пользователей"""
-        return self.title_video
+        return self.title
 
 
 class PLVideo(Video):
